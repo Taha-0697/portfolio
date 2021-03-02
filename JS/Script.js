617bee3528630d69812484f9b58491636da92f78
Taha-0697 <chgatai5.tc@gmail.com>
2021-03-02 21:21:11 +0500

    Updates

---
+++ JS/Script.js
@@ -0,0 +1,30 @@
+var aboutSection = document.getElementById("about-section");
+var skillsSection = document.getElementById("skills-section");
+var resumeSection = document.getElementById("resume-section");
+var worksSection = document.getElementById("works-section");
+var socialsSection = document.getElementById("socials-section");
+
+const menuLinks = document.querySelectorAll('.menu .menu-item');
+menuLinks.forEach(el => {
+    el.addEventListener('click', function () {
+
+        let sectionToGo = aboutSection;
+
+        switch (this.id) {
+            case 'skills':
+                sectionToGo = skillsSection;
+                break;
+            case 'resume':
+                sectionToGo = resumeSection;
+                break;
+            case 'works':
+                sectionToGo = worksSection;
+                break;
+            case 'socials':
+                    sectionToGo = socialsSection;
+                    break;
+        }
+        // Scroll smoothly to section
+        sectionToGo.scrollIntoView({behavior: 'smooth'});
+    })
+})
